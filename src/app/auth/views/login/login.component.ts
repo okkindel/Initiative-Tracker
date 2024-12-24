568a083734844d6a62ab3390c4a84ef0f54e0890
Maciej Hajduk <maciej.hajduk@deviniti.com>
2024-12-24 12:36:11 +0100

    build: bundle size

--- src/app/auth/views/login/login.component.ts
+++ src/app/auth/views/login/login.component.ts
@@ -1,5 +1,5 @@
 import { FormControl, Validators, FormGroup } from '@angular/forms';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { UserService } from '@api/services';
 import { Router } from '@angular/router';
@@ -8,10 +8,16 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   standalone: false,
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   private readonly _messageService = inject(MessageService);
   private readonly _userService = inject(UserService);
   private readonly _router = inject(Router);
+
+  public readonly isLoggingIn = signal<boolean>(false);
+
+  public ngOnInit(): void {
+    if (this._userService.getIsAuthenticated()) this._router.navigate(['/']);
+  }
 
   public readonly form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -20,14 +26,19 @@ export class LoginComponent {
 
   public login(): void {
     const { email, password } = this.form.value;
+    this.isLoggingIn.set(true);
+
     this._userService.login(email!, password!).subscribe({
+      complete: () => this.isLoggingIn.set(false),
       next: () => this._router.navigate(['/']),
-      error: (error) =>
+      error: (error) => {
+        this.isLoggingIn.set(false);
         this._messageService.add({
           detail: error.message,
           severity: 'error',
           life: 3000,
-        }),
+        });
+      },
     });
   }
 }
--- src/app/core/views/home/home.component.ts
+++ src/app/core/views/home/home.component.ts
@@ -6,6 +6,8 @@ import {
 } from '@angular/core';
 import { InitiativeDiceModalComponent } from '@core/modals';
 import { DialogService } from 'primeng/dynamicdialog';
+import { UserService } from '@api/services';
+import { Router } from '@angular/router';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,6 +16,8 @@ import { DialogService } from 'primeng/dynamicdialog';
 })
 export class HomeComponent {
   private readonly _dialogService = inject(DialogService);
+  private readonly _userService = inject(UserService);
+  private readonly _router = inject(Router);
 
   public readonly dialogVisible = signal(false);
 
@@ -22,5 +26,11 @@ export class HomeComponent {
     ref.onClose.subscribe((res) => console.log('closed', res));
     // this.dialogVisible.set(true);
     // setTimeout(() => this._diceBox()?.roll('1d10'), 1000);
+  }
+
+  public logout(): void {
+    this._userService.logout().subscribe(() => {
+      this._router.navigate(['/login']);
+    });
   }
 }
